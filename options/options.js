03346bed0eadf96f32bdcc8b75d007ad7a7b8a63
Crowdbotics <engineering@crowdbotics.com>
2024-07-17 09:00:03 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tesapp2-dev-136415",
+  url: "https://tesapp2-dev-136415.botics.co",
+  api: "https://tesapp2-dev-136415.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
